a6387faff796b41c56fa8282e947192ca9fd3100
NielsvandenDries <niels@openmindedit.nl>
2023-06-14 10:02:27 +0200

    update

--- components/Messages.php
+++ components/Messages.php
@@ -16,22 +16,4 @@ class Messages extends ComponentBase
     {
         return [];
     }
-
-    public function onSaveMessage()
-    {
-        $message = new Messages();
-        $message->subject = post('subject');
-        $message->message = post('message');
-        $message->save();
-
-        // Eventuele verdere logica of omleiding na het opslaan van het bericht
-
-        // Optioneel: Maak een succesmelding om te tonen aan de gebruiker
-        Flash::success('Bericht succesvol opgeslagen.');
-
-        // Optioneel: Omleiden naar een andere pagina na het opslaan van het bericht
-        // return Redirect::to('success-page');
-
-        trace_log('Model geladen: ' . get_class(new Messages()));
-    }
 }
