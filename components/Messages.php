91e119ee965249f15379272b0ae6ac1466bfb396
NielsvandenDries <niels@openmindedit.nl>
2023-06-14 10:13:41 +0200

    update

--- components/Messages.php
+++ components/Messages.php
@@ -1,6 +1,8 @@
 <?php namespace NielsVanDenDries\Messenger\Components;
 
 use Cms\Classes\ComponentBase;
+use Illuminate\Support\Facades\Auth;
+use RainLab\User\Models\User;
 
 class Messages extends ComponentBase
 {
@@ -16,4 +18,35 @@ class Messages extends ComponentBase
     {
         return [];
     }
+
+    protected $fillable = ['sender_id', 'recipient_id', 'content'];
+
+    public static function createMessage($senderId, $recipientId, $content)
+    {
+        $message = new self();
+        $message->sender_id = $senderId;
+        $message->recipient_id = $recipientId;
+        $message->content = $content;
+        $message->save();
+
+        return $message;
+    }
+
+    public function sendMessage()
+    {
+        $recipientId = post('recipient_id');
+        $messageContent = post('message_content');
+
+        $senderId = Auth::getUser()->id;
+
+        $message = Messages::createMessage($senderId, $recipientId, $messageContent);
+
+        Flash::success('Bericht succesvol verzonden!');
+    }
+
+    public function onRender()
+    {
+        $this->page['users'] = User::all();
+    }
+
 }
